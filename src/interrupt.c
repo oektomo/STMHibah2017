b6bb4e356821bc6d95dd495910f9fafbb225b963
oetomo <oektomo@gmail.com>
2017-10-28 22:22:46 +0700

    activating USART1 and USART3 interrupt receive then redirecting received data from USART1 to USART3 vice versa

--- src/interrupt.c
+++ src/interrupt.c
@@ -11,6 +11,11 @@
 
 //extern uint8_t RxBuffer;
 
+/*
+ * Receive interrupt configuration on NVIC
+ * We need to learn a lot about NVIC
+ * Receive interrupt for USART1 and USART3
+ */
 void
 NVIC_Config(void)
 {
@@ -18,6 +23,10 @@ NVIC_Config(void)
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
 
 	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStruct);
+
+	NVIC_InitStruct.NVIC_IRQChannel = USART3_IRQn;
 	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStruct);
 }
@@ -28,9 +37,20 @@ USART1_IRQHandler(void)
 	// check if Receive Data Register not empty
 	if( USART_GetITStatus(USARTrPi, USART_IT_RXNE) ) {
 		uint8_t RxBuffer = USART_ReceiveData(USARTrPi);
+		USART_Tx(USARTm8, RxBuffer);
 		RxBuffer++;
-		USART_Tx(USARTrPi, RxBuffer);
+		//USART_Tx(USARTrPi, RxBuffer);
 
 	}
 }
 
+void
+USART3_IRQHandler(void)
+{
+	// check if Receive Data Register not empty
+	if( USART_GetITStatus(USARTm8, USART_IT_RXNE) ) {
+		uint8_t RxBuffer = USART_ReceiveData(USARTm8);
+		USART_Tx(USARTrPi, RxBuffer);
+
+	}
+}
